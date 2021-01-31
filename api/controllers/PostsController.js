d2aa01ee3767ba0712ff5c428998de2955bcd889
Anthony Kim <anthony092777@gmail.com>
2021-01-30 19:50:04 -0500

    init

---
+++ api/controllers/PostsController.js
@@ -0,0 +1,51 @@
+// dummy database
+const post1 = {id: 1, 
+    title: 'POST TITLE 1', 
+    body: 'HERE IS MY BODY, DO WITH IT WHAT YOU WANT'}
+
+const post2 = {id: 2,
+    title: 'TITLE 2',
+    body: 'BODY BODY BODY'
+}
+
+const post3 = {id: 3,
+    title: 'TITLE 3',
+    body: 'BODY 3'
+
+}
+
+const allPosts = [post1, post2, post3]
+
+
+module.exports = {
+    posts: function(req, res) {
+        // json format is native to javascript 
+        
+        res.send(allPosts)
+        // command option s to save all the files 
+    },
+
+    create: function(req, res) {
+        const title = req.param('title')
+        const body = req.param('body')
+        console.log(title + " " + body)
+        // SVGPathSegList.log.debug(title + " " + body)
+        const newPosts = {id: 4, title: title, body: body}
+        allPosts.push(newPosts)
+
+        res.end()
+    },
+
+    findById: function(req, res) {
+        const postId = req.param('postId')
+
+        const filteredPosts = allPosts.filter(p => {return p.id == postId})
+
+        if(filteredPosts.length > 0){
+            res.send(filteredPosts[0])
+        }else{
+            res.send('Failed to find post by id: ' + postId)
+        }
+        res.send(postId)
+    }
+}
